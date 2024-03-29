d835ccefc7df28073833c3d9778507f52acf842f
Jon Schwamman <jschwamman@kpa.io>
2024-03-29 11:13:19 -0700

    Build dist

--- dist/spectrum-integration/function/model/kpa-spectrum-configuration-model.d.ts
+++ dist/spectrum-integration/function/model/kpa-spectrum-configuration-model.d.ts
@@ -1,6 +1,11 @@
 import { Document } from "mongodb";
 import { KPABaseConfigurationModel } from "../../../base-integration/src/model";
 export declare class KPASpectrumConfigurationModel extends KPABaseConfigurationModel {
+    authType: string;
+    spectrumUrl: string;
+    spectrumPort: number;
+    authorizationId: string;
+    companyCodes: string[];
     clientId: string;
     clientSecret: string;
     constructor(data: Document);
