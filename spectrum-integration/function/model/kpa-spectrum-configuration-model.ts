767def98ba7006e6170c18767559f1e6df07d718
Jon Schwamman <50553064+schwamman@users.noreply.github.com>
2024-03-29 11:32:33 -0700

    Update spectrum config (#15)
    * Added properties to the spectrum config
    
    * Build dist

--- spectrum-integration/function/model/kpa-spectrum-configuration-model.ts
+++ spectrum-integration/function/model/kpa-spectrum-configuration-model.ts
@@ -2,11 +2,22 @@ import { Document } from "mongodb";
 import { KPABaseConfigurationModel } from "../../../base-integration/src/model";
 
 export class KPASpectrumConfigurationModel extends KPABaseConfigurationModel {
+    authType: string;
+    spectrumUrl: string;
+    spectrumPort: number;
+    authorizationId: string;
+    companyCodes: string[];
     clientId: string;
     clientSecret: string;
 
     constructor(data: Document) {
-        super(data)
+        super(data);
+
+        this.authType = data['auth_type'];
+        this.spectrumUrl = data['spectrum_url'];
+        this.spectrumPort = data['spectrum_port'];
+        this.authorizationId = data['authorization_id'];
+        this.companyCodes = data['company_codes'];
         this.clientId = data['client_id'];
         this.clientSecret = data['client_secret'];
     }
@@ -14,6 +25,11 @@ export class KPASpectrumConfigurationModel extends KPABaseConfigurationModel {
     syncChanges():Document {
         super.syncChanges();
 
+        this.doc['auth_type'] = this.authType;
+        this.doc['spectrum_url'] = this.spectrumUrl;
+        this.doc['spectrum_port'] = this.spectrumPort;
+        this.doc['authorization_id'] = this.authorizationId;
+        this.doc['company_codes'] = this.companyCodes;
         this.doc['client_id'] = this.clientId;
         this.doc['client_secret'] = this.clientSecret;
 
