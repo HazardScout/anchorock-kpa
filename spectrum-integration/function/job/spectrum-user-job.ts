ba987388d817e09e94562cb456aa4024b914f7a8
ymarsal-kpa <159466939+ymarsal-kpa@users.noreply.github.com>
2024-04-15 21:02:33 +0700

    fix:Trim spectrum employee number (#22)
    * fix:Trim spectrum employee number
    
    * rebuild dist
    
    * Update trim function
    
    ---------
    
    Co-authored-by: yogi <yogi@anchorock.com>

--- spectrum-integration/function/job/spectrum-user-job.ts
+++ spectrum-integration/function/job/spectrum-user-job.ts
@@ -52,7 +52,7 @@ export class SpectrumUserJob implements IJob {
                     var kpaUser : KPAUserModel | null = null;
                     for (let i = 0; i < kpaExistUsers.length; i++) {
                         const kpaExistUser = kpaExistUsers[i];
-                        const employeeCode = `${companyCode}-${user.employeeCode}`;
+                        const employeeCode = `${companyCode.trim()}-${user.employeeCode.trim()}`;
                         if (kpaExistUser.employeeNumber === employeeCode) {
                             kpaUser = kpaExistUser;
                             kpaExistUsers.splice(i,1);
@@ -81,10 +81,10 @@ export class SpectrumUserJob implements IJob {
                     }
 
                     //Create Users 
-                    kpaUser.employeeNumber = `${companyCode}-${user.employeeCode}`;
+                    kpaUser.employeeNumber = `${companyCode.trim()}-${user.employeeCode.trim()}`;
                     kpaUser.firstName = user.firstName
                     kpaUser.lastName = user.lastName;
-                    kpaUser.username = `${companyCode}-${user.employeeCode}`;
+                    kpaUser.username = kpaUser.employeeNumber;
                     kpaUser.email = '';
                     kpaUser.initialPassword = `KPAFlex2024!!`;
                     kpaUser.role = this.defaultRole;
