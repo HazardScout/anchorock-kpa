b8d3a57032a53a9440abc53a22e09970cad8fefd
Jon Schwamman <50553064+schwamman@users.noreply.github.com>
2024-09-26 12:12:56 -0700

    Fix procore integration (#41)
    * Fixed default role for spectrum users integration
    
    * Fixed regex to check for new line chracters
    
    * Build dist

--- base-integration/src/utilities/helper.ts
+++ base-integration/src/utilities/helper.ts
@@ -6,7 +6,8 @@ export class Helper {
 
     let finalData = data.trim();
 
-    const regex = /\/r|\/n/g
+    // Must check for both text characters ('\\r\\n') and actual unicode ('\r\n')
+    const regex = /\\r\\n|\r\n|\\r|\\n|\r|\n/g
     finalData = finalData.replace(regex, ' ')
     if (
       finalData.indexOf(',') < 0
