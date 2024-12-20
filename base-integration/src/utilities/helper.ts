3074b684743927f0145ae29c79b1867ca41dacc5
Tona Dansie <tdansie@kpa.io>
2024-12-20 11:40:44 -0700

    Initial Commit - add function to convert epoch times to date string for dataloading

--- base-integration/src/utilities/helper.ts
+++ base-integration/src/utilities/helper.ts
@@ -1,5 +1,5 @@
 export class Helper {
-  static csvContentChecker(data:string) : string {
+  static csvContentChecker(data: string): string {
     if (data == null) {
       return '';
     }
@@ -7,16 +7,29 @@ export class Helper {
     let finalData = String(data).trim();
 
     // Must check for both text characters ('\\r\\n') and actual unicode ('\r\n')
-    const regex = /\\r\\n|\r\n|\\r|\\n|\r|\n/g
-    finalData = finalData.replace(regex, ' ')
-    if (
-      finalData.indexOf(',') < 0
-      && finalData.indexOf('"') < 0
-    ) {
+    const regex = /\\r\\n|\r\n|\\r|\\n|\r|\n/g;
+    finalData = finalData.replace(regex, ' ');
+    if (finalData.indexOf(',') < 0 && finalData.indexOf('"') < 0) {
       return finalData;
     }
 
     finalData = finalData.replace(/"/g, '""');
     return `"${finalData}"`;
+  }
+
+  static formatDatesIfEpoch(incomingDate: string | number): string {
+    if (!incomingDate) {
+      return '';
+    }
+    // if epoch time
+    if (
+      typeof incomingDate === 'number' &&
+      incomingDate.toString().length >= 10
+    ) {
+      const date = new Date(incomingDate);
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+    // Return the original value if not epoch time
+    return incomingDate as string;
   }
 }
